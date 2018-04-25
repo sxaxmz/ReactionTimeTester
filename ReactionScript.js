355a4395cede0d90f6c7440aa8e8aa347f6650c0
Osama Hussein <osamathadon@gmail.com>
2018-04-25 14:10:17 +0800

    Initial layout

---
+++ ReactionScript.js
@@ -0,0 +1,80 @@
+        var audio = new Audio('melody.mp3');
+		audio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+        }, false);
+		audio.play();
+
+		var startTime = new Date().getTime();
+		var clicks = 0;
+		var lowestScore= 0;
+
+		function getRandomColor(){
+			var letters = '0123456789ABCDEF'.split('');
+			var color = '#';
+
+			for (var i = 0; i < 6; i++) {
+				color += letters[Math.floor(Math.random() * 16)]
+			}
+
+			return color;
+		}
+
+		function shapeAppear(){
+			var top = Math.random() * 400;
+			var left = Math.random() * 400;
+			var width = (Math.random() * 200) + 100;
+			var height = (Math.random() * 200) + 100;
+
+			if (Math.random() > 0.5){
+				document.getElementById("shape").style.borderRadius = "50%";
+			} else {
+				document.getElementById("shape").style.borderRadius = "0";
+			}
+
+			document.getElementById("shape").style.top =  top + "px";
+			document.getElementById("shape").style.left = left + "px";
+			document.getElementById("shape").style.width =  width + "px";
+			document.getElementById("shape").style.height = height + "px";
+			document.getElementById("shape").style.backgroundColor =  getRandomColor();
+
+			document.getElementById("shape").style.display = "block";
+
+			startTime = new Date().getTime();
+		}
+
+		function appearAfter() {
+			setTimeout(shapeAppear, Math.random() * 2000);
+		}
+
+		appearAfter();
+		
+		document.getElementById("shape").onclick = function () {
+			++clicks;
+
+			if (clicks > 0){
+				document.getElementById("numberClicks").innerHTML = clicks;
+			}
+
+			document.getElementById("shape").style.display = "none";
+
+			var endTime = new Date().getTime();
+
+			var timeTaken = (startTime - endTime) / 1000;
+
+			if (lowestScore == 0) {
+				lowestScore = Math.abs(timeTaken);
+				document.getElementById("lowestScore").innerHTML = lowestScore + "s";
+			} 
+
+			if (lowestScore < Math.abs(timeTaken)) {
+
+				lowestScore = Math.abs(timeTaken);
+				document.getElementById("lowestScore").innerHTML = lowestScore + "s";
+			} 
+
+			document.getElementById("timeTaken").innerHTML = Math.abs(timeTaken) + "s";
+
+			appearAfter();
+
+		}
